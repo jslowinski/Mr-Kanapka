b752e5ee7b8c5ba78f42cb7ca24d61fb021e401a
jslowinski <40072977+jslowinski@users.noreply.github.com>
2018-12-07 23:39:14 +0100

    ver 0.1

---
+++ app/src/main/java/com/swapi/swapikotlin/DraftFragment.kt
@@ -0,0 +1,13 @@
+package com.swapi.swapikotlin
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class DraftFragment : Fragment(){
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.draft_layout,null)
+    }
+}
--- app/src/main/java/com/swapi/swapikotlin/Main2Activity.kt
+++ app/src/main/java/com/swapi/swapikotlin/Main2Activity.kt
@@ -1,85 +1,61 @@
 package com.swapi.swapikotlin
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
-import android.support.v4.view.GravityCompat
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
+import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import kotlinx.android.synthetic.main.activity_main2.*
-import kotlinx.android.synthetic.main.app_bar_main2.*
+import android.view.View
+import android.support.v7.widget.Toolbar
 
-class Main2Activity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+class Main2Activity : AppCompatActivity() {
+
+    lateinit var mDrawerLayout: DrawerLayout
+    lateinit var mNavigationView: NavigationView
+    lateinit var mFragmentManager: FragmentManager
+    lateinit var mFragmentTransaction: FragmentTransaction
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        mDrawerLayout = findViewById<View>(R.id.drawerLayout) as DrawerLayout
+        mNavigationView = findViewById<View>(R.id.navView) as NavigationView
+
+
+        mFragmentManager = supportFragmentManager
+        mFragmentTransaction = mFragmentManager.beginTransaction()
+        mFragmentTransaction.replace(R.id.containerView, TabFragment()).commit()
+
+        mNavigationView.setNavigationItemSelectedListener {
+            menuItem -> mDrawerLayout.closeDrawers()
+
+            if(menuItem.itemId==R.id.nav_item_inbox){
+                val ft = mFragmentManager.beginTransaction()
+                ft.replace(R.id.containerView, TabFragment()).commit()
+            }
+
+            if(menuItem.itemId==R.id.nav_item_sent){
+                val ft = mFragmentManager.beginTransaction()
+                ft.replace(R.id.containerView, SentFragment()).commit()
+            }
+
+            if(menuItem.itemId==R.id.nav_item_draft){
+                val ft = mFragmentManager.beginTransaction()
+                ft.replace(R.id.containerView, DraftFragment()).commit()
+            }
+
+            false
         }
 
-        val toggle = ActionBarDrawerToggle(
-            this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close
-        )
-        drawer_layout.addDrawerListener(toggle)
-        toggle.syncState()
+        val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
+        val mDrawerToggle = ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.app_name, R.string.app_name)
 
-        nav_view.setNavigationItemSelectedListener(this)
-    }
-
-    override fun onBackPressed() {
-        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
-            drawer_layout.closeDrawer(GravityCompat.START)
-        } else {
-            super.onBackPressed()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main2, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        when (item.itemId) {
-            R.id.action_settings -> return true
-            else -> return super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        // Handle navigation view item clicks here.
-        when (item.itemId) {
-            R.id.nav_camera -> {
-                // Handle the camera action
-            }
-            R.id.nav_gallery -> {
-
-            }
-            R.id.nav_slideshow -> {
-
-            }
-            R.id.nav_manage -> {
-
-            }
-            R.id.nav_share -> {
-
-            }
-            R.id.nav_send -> {
-
-            }
-        }
-
-        drawer_layout.closeDrawer(GravityCompat.START)
-        return true
+        mDrawerLayout.setDrawerListener(mDrawerToggle)
+        mDrawerToggle.syncState()
     }
 }
